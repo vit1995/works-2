83ceba190285d3bd0884321e6bb348394c117a8e
Гавриков Виталий Игоревич <gavrikov131313@gmail.com>
2025-11-19 22:42:45 +0300

    Updates

--- js/dev/app.js
+++ js/dev/app.js
@@ -12013,3 +12013,18 @@ function formInit() {
   formFieldsInit();
 }
 document.querySelector("[data-fls-form]") ? window.addEventListener("load", formInit) : null;
+document.addEventListener("DOMContentLoaded", function() {
+  const navButton = document.querySelector(".nav");
+  const pageHeight = document.body.scrollHeight;
+  const triggerPoint = pageHeight * 0.5;
+  window.addEventListener("scroll", function() {
+    const scrollProgress = window.scrollY / triggerPoint;
+    if (scrollProgress >= 1) {
+      navButton.style.opacity = "1";
+      navButton.style.visibility = "visible";
+    } else {
+      navButton.style.opacity = "0";
+      navButton.style.visibility = "hidden";
+    }
+  });
+});
--- js/dev/index.js
+++ js/dev/index.js
@@ -1,20 +1,5 @@
 import "./app.min.js";
 document.addEventListener("DOMContentLoaded", function() {
-  const navButton = document.querySelector(".nav");
-  const pageHeight = document.body.scrollHeight;
-  const triggerPoint = pageHeight * 0.5;
-  window.addEventListener("scroll", function() {
-    const scrollProgress = window.scrollY / triggerPoint;
-    if (scrollProgress >= 1) {
-      navButton.style.opacity = "1";
-      navButton.style.visibility = "visible";
-    } else {
-      navButton.style.opacity = "0";
-      navButton.style.visibility = "hidden";
-    }
-  });
-});
-document.addEventListener("DOMContentLoaded", function() {
   const videoContainers = document.querySelectorAll(".video-container");
   videoContainers.forEach((container) => {
     const preview = container.querySelector(".video-preview");

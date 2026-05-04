44103f21197dcac4ce010e3781064437527acafc
vit1995 <111640998+vit1995@users.noreply.github.com>
2026-05-04 22:00:38 +0300

    Updates

--- js/dev/index.js
+++ js/dev/index.js
@@ -6669,12 +6669,16 @@ function initSliders() {
   }
   if (document.querySelector(".steaps__slider")) {
     new Swiper(".steaps__slider", {
-      modules: [Pagination, Scrollbar],
+      modules: [Pagination, Scrollbar, Navigation],
       observer: true,
       observeParents: true,
       slidesPerView: 1,
       spaceBetween: 10,
       speed: 800,
+      navigation: {
+        prevEl: ".steaps-button-prev",
+        nextEl: ".steaps-button-next"
+      },
       scrollbar: {
         el: ".swiper-scrollbar",
         draggable: true
@@ -13361,59 +13365,6 @@ function rippleEffect() {
   });
 }
 document.querySelector("[data-fls-ripple]") ? window.addEventListener("load", rippleEffect) : null;
-function preloader() {
-  const preloaderImages = document.querySelectorAll("img");
-  const htmlDocument = document.documentElement;
-  const isPreloaded = localStorage.getItem(location.href) && document.querySelector('[data-fls-preloader="true"]');
-  if (preloaderImages.length && !isPreloaded) {
-    let setValueProgress2 = function(progress2) {
-      showPecentLoad ? showPecentLoad.innerText = `${progress2}%` : null;
-      showLineLoad ? showLineLoad.style.width = `${progress2}%` : null;
-    }, imageLoaded2 = function() {
-      imagesLoadedCount++;
-      progress = Math.round(100 / preloaderImages.length * imagesLoadedCount);
-      const intervalId = setInterval(() => {
-        counter >= progress ? clearInterval(intervalId) : setValueProgress2(++counter);
-        counter >= 100 ? addLoadedClass() : null;
-      }, 10);
-    };
-    var setValueProgress = setValueProgress2, imageLoaded = imageLoaded2;
-    const preloaderTemplate = `
-			<div class="fls-preloader">
-				<div class="fls-preloader__body">
-					<div class="fls-preloader__counter">0%</div>
-					<div class="fls-preloader__line"><span></span></div>
-				</div>
-			</div>`;
-    document.body.insertAdjacentHTML("beforeend", preloaderTemplate);
-    document.querySelector(".fls-preloader");
-    const showPecentLoad = document.querySelector(".fls-preloader__counter"), showLineLoad = document.querySelector(".fls-preloader__line span");
-    let imagesLoadedCount = 0;
-    let counter = 0;
-    let progress = 0;
-    htmlDocument.setAttribute("data-fls-preloader-loading", "");
-    htmlDocument.setAttribute("data-fls-scrolllock", "");
-    preloaderImages.forEach((preloaderImage) => {
-      const imgClone = document.createElement("img");
-      if (imgClone) {
-        imgClone.onload = imageLoaded2;
-        imgClone.onerror = imageLoaded2;
-        preloaderImage.dataset.src ? imgClone.src = preloaderImage.dataset.src : imgClone.src = preloaderImage.src;
-      }
-    });
-    setValueProgress2(progress);
-    const preloaderOnce = () => localStorage.setItem(location.href, "preloaded");
-    document.querySelector('[data-fls-preloader="true"]') ? preloaderOnce() : null;
-  } else {
-    addLoadedClass();
-  }
-  function addLoadedClass() {
-    htmlDocument.setAttribute("data-fls-preloader-loaded", "");
-    htmlDocument.removeAttribute("data-fls-preloader-loading");
-    htmlDocument.removeAttribute("data-fls-scrolllock");
-  }
-}
-document.addEventListener("DOMContentLoaded", preloader);
 (function() {
   const calculator = document.querySelector("[data-fls-calc]");
   if (!calculator) return;
